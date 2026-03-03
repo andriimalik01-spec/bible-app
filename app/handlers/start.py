c188777e8fa110d6b8d681e0552fafa79b97a672
andrii.malik <andrii.malik01@gmail.com>
2026-03-03 13:25:24 +0100

    feat: add user registration and initialize readeing progress on /start

--- app/handlers/start.py
+++ app/handlers/start.py
@@ -1,9 +1,22 @@
 from aiogram import Router
-from aiogram.filters import Command
 from aiogram.types import Message
+from aiogram.filters import Command
+
+from app.services.users import create_user_if_not_exists
 
 router = Router()
 
+
 @router.message(Command("start"))
 async def start_handler(message: Message):
-    await message.answer("Бот запущено.")
+
+    await create_user_if_not_exists(
+        telegram_id=message.from_user.id,
+        username=message.from_user.username,
+        first_name=message.from_user.first_name
+    )
+
+    await message.answer(
+        "📖 Ласкаво просимо до Bible Bot!\n\n"
+        "Нехай кожен день буде кроком ближче до Бога 🙏"
+    )
---
+++ app/services/users.py
@@ -0,0 +1,42 @@
+from app.database.connection import get_pool
+
+
+async def create_user_if_not_exists(
+    telegram_id: int,
+    username: str | None,
+    first_name: str | None
+):
+    pool = get_pool()
+
+    async with pool.acquire() as conn:
+
+        user = await conn.fetchrow(
+            "SELECT id FROM users WHERE telegram_id = $1",
+            telegram_id
+        )
+
+        if user:
+            return user["id"]
+
+        # створюємо користувача
+        new_user = await conn.fetchrow(
+            """
+            INSERT INTO users (telegram_id, username, first_name)
+            VALUES ($1, $2, $3)
+            RETURNING id
+            """,
+            telegram_id,
+            username,
+            first_name
+        )
+
+        # створюємо прогрес
+        await conn.execute(
+            """
+            INSERT INTO reading_progress (user_id)
+            VALUES ($1)
+            """,
+            new_user["id"]
+        )
+
+        return new_user["id"]
