f0a9836d87435a0a88dd04f76b41619ffcae7f17
andrii.malik <andrii.malik01@gmail.com>
2026-03-03 14:34:54 +0100

    fix router

--- app/handlers/start.py
+++ app/handlers/start.py
@@ -1,7 +1,16 @@
+from aiogram import Router
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+from app.services.users import create_user_if_not_exists
 from app.keyboards.menus import main_menu
+
+router = Router()
+
 
 @router.message(CommandStart())
 async def start_handler(message: Message):
+
     await create_user_if_not_exists(
         message.from_user.id,
         message.from_user.username,
