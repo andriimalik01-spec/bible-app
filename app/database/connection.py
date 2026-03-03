00a0228366ec64db6aaa40a570a2413938c108e4
andrii.malik <andrii.malik01@gmail.com>
2026-03-03 12:38:41 +0100

    add postgres support

---
+++ app/database/connection.py
@@ -0,0 +1,13 @@
+import asyncpg
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+pool = None
+
+async def init_db():
+    global pool
+    pool = await asyncpg.create_pool(DATABASE_URL)
+
+async def get_pool():
+    return pool
